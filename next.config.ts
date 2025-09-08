797987c7cd98bca11967228681533e92b4d6a1c7
usermat4eso <your-email@example.com>
2025-09-09 01:33:22 +0200

    build: Desactivar errores de ESLint durante el build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
 
-const nextConfig: NextConfig = {
-  /* config options here */
+  // --- AÑADE ESTA SECCIÓN ---
+  eslint: {
+    // Warning: Esto desactivará los errores de ESLint durante el build.
+    // Es útil para desplegar rápidamente, pero se recomienda arreglar los errores.
+    ignoreDuringBuilds: true,
+  },
+  // --------------------------
 };
 
 export default nextConfig;
